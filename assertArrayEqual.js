eeb4c5255acbdfb6e15e7943b0be2a46366dfcf8
Ammar <ammar.kusow2@gmail.com>
2021-10-27 22:53:30 -0400

    StartCommitingMan

--- assertArrayEqual.js
+++ assertArrayEqual.js
@@ -1,39 +1,10 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅ this is a match ${actual} === ${expected}`);
-  } else {
-    console.log(`❌ this is not a match ${actual} !== ${expected}`);
-  }
-};
+const eqArrays = require('./eqArrays');
 
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length)
-    return false;
-  for (let i = arr1.length; i--;) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
 
-const eqObjects = function (object1, object2) {
-  if (Object.keys(object1).length !== Object.keys(object2).length) {
-    return false;
+const assertArraysEqual = function (arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅${arr1} === ${arr2}`)
   } else {
-    for (let i in object1) {
-      if ((Array.isArray(object1[i]) && Array.isArray(object2[i]))) {
-        if (eqArrays(object1[i], object2[i]) === false) {
-          return false
-        }
-      } else {
-        if (object1[i] !== object2[i]) {
-          return false;
-        }
-      }
-    }
-    return true;
+    console.log(`🛑 ${arr1} !== ${arr2}`)
   }
 }
-  
-
