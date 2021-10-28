1a54f42fdc3fce3dbed75812c2c86b2002caa9c3
Ammar <ammar.kusow2@gmail.com>
2021-10-27 21:55:24 -0400

    forgotToCommitAgain

---
+++ letterPositions.js
@@ -0,0 +1,14 @@
+const countLetters = function (words) {
+  const obj = {} // this is where the letters are stored
+   for (let i =0; i < words.length; i ++) { // looping through the string
+    let letter = words[i]; // declaring letter as each index in the string 
+   if (obj[letter]) { // if statment to see if obj(letter)
+     obj[letter] ++ 
+   }
+   else {
+     obj[letter] = 1
+   }
+   }
+ return obj
+ } 
+ console.log (countLetters("lighthouse in the house"))
