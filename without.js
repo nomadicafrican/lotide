b784ed9a9f958d5392ebe606f3e656e59e77373c
Ammar <ammar.kusow2@gmail.com>
2021-10-27 22:55:56 -0400

    committed

---
+++ without.js
@@ -0,0 +1,30 @@
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length)
+    return false;
+  for (let i = arr1.length; i--;) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function (arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅${arr1} === ${arr2}`)
+  } else {
+    console.log(`🛑 ${arr1} !== ${arr2}`)
+  }
+}
+
+const without = function(source, itemsToRemove) {
+  for (let i = 0; i < source.length; i++) {
+    for (let j = 0; j < itemsToRemove.length; j++) {
+      if (source[i] === itemsToRemove[j]) {
+        source.splice(i, 1);
+      }
+    }
+  }
+  return source;
+}
+
