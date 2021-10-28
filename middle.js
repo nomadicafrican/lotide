9889742a26c9a3da1c08e74a495d20d3d853e0e1
Ammar <ammar.kusow2@gmail.com>
2021-10-27 21:27:03 -0400

    MadeEmptyArray

--- middle.js
+++ middle.js
@@ -1,17 +1,4 @@
-function middle(arr) {
-    
-  if (arr.length < 3) {
-    return [];
-    
-      
-  } else if (arr.length % 2 === 0) {
-    return [arr[Math.round(arr.length / 2) - 1], arr[Math.round(arr.length / 2)]];
-     
-  
-    
-  
-  } else {
-    return [arr[Math.floor(arr.length / 2)]];
-  }
-    
+const middle = (inputArray) => {
+  const middleArray = [];
+
 }
