2c6ed36b0a3617a038ad7ad3d8244b7913fc4e3e
Ammar <ammar.kusow2@gmail.com>
2021-10-27 21:23:42 -0400

    needToCommitMoreOften

---
+++ eqArrays.js
@@ -0,0 +1,15 @@
+const assertEqual = require('./assertEqual');
+
+let eqArrays = function(arr1, arr2) {
+  if (arr1.length != arr2.length) {
+  return false;
+}
+
+for (i = 0; i < arr1.length; i ++) {
+  if (arr1[i] != arr2[i]) {
+    return false;
+  }
+}
+return true;
+}
+
--- letterPositions.js
+++
@@ -1,5 +0,0 @@
-const letterPositions = function(sentence) {
-  const results = {};
-  // logic to update results here
-  return results;
-};
